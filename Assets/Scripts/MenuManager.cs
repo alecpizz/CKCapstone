75b9aacaf15dcb9963020d3cc354d67d54b9b11c
RiderH <114954577+RiderH@users.noreply.github.com>
2024-09-26 11:22:10 -0500

    Created a menu manager script for the main menu
    Only works if the play scene is scene "1"
    Also contains a function for going back to the main menu which only works if the main menu is scene "0"

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    /******************************************************************
+*    Author: Rider Hagen
+*    Contributors: [people who edited file, add your name if you edit]
+*    Date Created: 9/26/24
+*    Description: This is just meant to make menu buttons, when pressed, work.
+*******************************************************************/
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
