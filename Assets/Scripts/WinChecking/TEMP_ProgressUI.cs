33abba0f3b7d7684e9c46aa612e665081b728d2e
NGrinstead0101 <ngrinstead01@gmail.com>
2024-09-28 15:56:08 -0500

    Comments

--- Assets/Scripts/WinChecking/TEMP_ProgressUI.cs
+++ Assets/Scripts/WinChecking/TEMP_ProgressUI.cs
@@ -1,3 +1,10 @@
+/******************************************************************
+*    Author: Nick Grinstead
+*    Contributors: 
+*    Date Created: 9/28/24
+*    Description: Temporary script that updates UI to reflect the 
+*       player's progress with collecting the correct melody.
+*******************************************************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,6 +24,9 @@ public class TEMP_ProgressUI : MonoBehaviour
     private const string BaseSequenceText = "Melody:";
     private const string BaseCollectedText = "Collected Notes:";
 
+    /// <summary>
+    /// Initializing values and registering to actions
+    /// </summary>
     private void Start()
     {
         if (_messageWaitTime <= 0) { _messageWaitTime = 5f; }
@@ -36,6 +46,9 @@ public class TEMP_ProgressUI : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Unregistering from actions
+    /// </summary>
     private void OnDisable()
     {
         WinChecker.CollectedNote -= UpdateCollectedNotesText;
@@ -43,16 +56,26 @@ public class TEMP_ProgressUI : MonoBehaviour
         WinChecker.GotWrongSequence -= DisplayIncorrectMessage;
     }
 
+    /// <summary>
+    /// Display newly collected note in UI onces collected
+    /// </summary>
+    /// <param name="collectedNote">new note as int</param>
     private void UpdateCollectedNotesText(int collectedNote)
     {
         _collectedNotesUI.text += " " + collectedNote;
     }
 
+    /// <summary>
+    /// Returns collected notes UI to its default text
+    /// </summary>
     private void ResetCollectedNotesText()
     {
         _collectedNotesUI.text = BaseCollectedText;
     }
 
+    /// <summary>
+    /// Invoked to display the wrong sequence screen
+    /// </summary>
     private void DisplayIncorrectMessage()
     {
         ResetCollectedNotesText();
@@ -62,12 +85,19 @@ public class TEMP_ProgressUI : MonoBehaviour
         StartCoroutine(DisplayTimedMessage(_incorrectMessage));
     }
 
+    /// <summary>
+    /// Displays message that door has been unlocked
+    /// </summary>
     private void DisplayDoorUnlockMessage()
     {
-        _incorrectMessage.SetActive(false);
         _doorUnlockMessage.SetActive(true);
     }
 
+    /// <summary>
+    /// Displays a screen or message for a short time before resetting scene
+    /// </summary>
+    /// <param name="message">UI to toggle on</param>
+    /// <returns>Waits for seconds based on message wait time</returns>
     private IEnumerator DisplayTimedMessage(GameObject message)
     {
         message.SetActive(true);
