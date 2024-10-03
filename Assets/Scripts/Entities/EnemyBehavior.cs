e5b60eeaad67fb7cb489fb74eb1f8da28cfdeb3a
Raccoon0223 <colestran@gmail.com>
2024-10-03 11:46:55 -0500

    Basic Setup

---
+++ Assets/Scripts/Entities/EnemyBehavior.cs
@@ -0,0 +1,26 @@
+/******************************************************************
+*    Author: Cole Stranczek
+*    Contributors: Cole Stranczek, Mitchell Young
+*    Date Created: 10/3/24
+*    Description: Script that handles the behavior of the enemy,
+*    from movement to causing a failstate with the player
+*******************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
