b976510819f4fd45e704a20899925eea00216f3a
dgalmines <123035687+dgalmines@users.noreply.github.com>
2024-10-10 12:32:44 -0500

    made the max Y scale and min Y scale serialized fields

--- Assets/Scripts/PlayerArrows.cs
+++ Assets/Scripts/PlayerArrows.cs
@@ -16,6 +16,12 @@ public class PlayerArrows : MonoBehaviour
     //The speed at which you want the arrows to shrink
     [SerializeField] private float _pulseSpeed;
 
+    //The minimum size an arrow can pulsate to
+    [SerializeField] private float _minYScale;
+
+    //The maximum size an arrow can pulsate to
+    [SerializeField] private float _maxYScale;
+
     //The 4 arrow objects
     [SerializeField] private GameObject _arrow1;
     [SerializeField] private GameObject _arrow2;
@@ -25,25 +31,16 @@ public class PlayerArrows : MonoBehaviour
     //The variable calculated from BPM, beats per second
     private float timingBetweenEachBeat;
 
-    //The minimum size an arrow can pulsate to
-    private float minYScale;
-
-    //The maximum size an arrow can pulsate to
-    private float maxYScale;
-
     //Is the pulsating coroutine allowed to restart?
     private bool canRestartCoroutine;
 
     /// <summary>
-    /// Calculates the beats per second from BPM, starts the 
-    /// pulse coroutine, and assigns min and max scale values.
+    /// Calculates the beats per second from BPM, & starts the 
+    /// pulse coroutine.
     /// </summary>
     void Start()
     {
         timingBetweenEachBeat = _bpm / 60;
-        minYScale = 0.5f;
-        maxYScale = 1f;
-
         StartCoroutine("Pulse");
     }
 
@@ -63,28 +60,28 @@ public class PlayerArrows : MonoBehaviour
         if (!canRestartCoroutine)
         {
             //if arrow 1 is not at its minimum scale...
-            if (scale1.y >= minYScale)
+            if (scale1.y >= _minYScale)
             {
                 //...shrink its scale
                 _arrow1.transform.localScale -= new Vector3(0, 
                     _pulseSpeed * 0.1f, 0f);
             }
             //if arrow 2 is not at its minimum scale...
-            if (scale2.y >= minYScale)
+            if (scale2.y >= _minYScale)
             {
                 //...shrink its scale
                 _arrow2.transform.localScale -= new Vector3(0,
                     _pulseSpeed * 0.1f, 0f);
             }
             //if arrow 3 is not at its minimum scale...
-            if (scale3.y >= minYScale)
+            if (scale3.y >= _minYScale)
             {
                 //...shrink its scale
                 _arrow3.transform.localScale -= new Vector3(0,
                    _pulseSpeed * 0.1f, 0f);
             }
             //if arrow 4 is not at its minimum scale...
-            if (scale4.y >= minYScale)
+            if (scale4.y >= _minYScale)
             {
                 //...shrink its scale
                 _arrow4.transform.localScale -= new Vector3(0,
@@ -109,15 +106,16 @@ public class PlayerArrows : MonoBehaviour
         //coroutine is active
         canRestartCoroutine = false;
         //grow the arrows to max size
-        _arrow1.transform.localScale = new Vector3(0.23f, maxYScale, 
+        _arrow1.transform.localScale = new Vector3(0.23f, _maxYScale, 
             0.63f);
-        _arrow2.transform.localScale = new Vector3(0.23f, maxYScale, 
+        _arrow2.transform.localScale = new Vector3(0.23f, _maxYScale, 
             0.63f);
-        _arrow3.transform.localScale = new Vector3(0.23f, maxYScale, 
+        _arrow3.transform.localScale = new Vector3(0.23f, _maxYScale, 
             0.63f);
-        _arrow4.transform.localScale = new Vector3(0.23f, maxYScale, 
+        _arrow4.transform.localScale = new Vector3(0.23f, _maxYScale, 
             0.63f);
         //wait depending on the BPM to end the coroutine
+        //beats per second is converted to seconds per beat
         yield return new WaitForSeconds(1 / timingBetweenEachBeat);
         //coroutine is no longer active
         canRestartCoroutine = true;
