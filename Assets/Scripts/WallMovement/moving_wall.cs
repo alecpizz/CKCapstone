5991c38e299c0850db235a00ac452f18866ef2f0
Josie <jqualls@mail.bradley.edu>
2024-10-11 17:21:55 -0500

    Finished wall movement prototype
    Color coded walls to switches
    
    Ghost wall indicates where walls will go
    
    Player can move where walls were

--- Assets/Scripts/WallMovement/moving_wall.cs
+++ Assets/Scripts/WallMovement/moving_wall.cs
@@ -1,28 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class moving_wall : MonoBehaviour
+public class moving_wall : MonoBehaviour, IGridEntry
 {
     private Vector3 originTransform;
 
     [SerializeField] private Vector3 wallTransform;
     [SerializeField] private Material wallColor;
 
+    [SerializeField] private GridPlacer wallGrid;
+    [SerializeField] private GameObject wallGhost;
+
+    public bool IsTransparent => throw new System.NotImplementedException();
+
+
+    public GameObject GetGameObject => throw new System.NotImplementedException();
+
+    public Vector3 Position => throw new System.NotImplementedException();
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         originTransform = transform.position;
+
+        wallGhost.transform.position = wallTransform;
     }
 
     public void Wall_Is_Moved()
     {
         transform.position = wallTransform;
-        
+        wallGhost.transform.position = originTransform;
+        wallGrid.UpdatePosition();
     }
 
     public void Wall_Move_Back()
     {
         transform.position = originTransform;
+        wallGhost.transform.position = wallTransform;
+        wallGrid.UpdatePosition();
     }
 }
