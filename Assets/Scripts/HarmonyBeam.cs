1f6f768d59061dd1a6def1adfa3ff1086e036757
real-claire <clairenoto0@gmail.com>
2024-10-12 12:10:22 -0500

    Harmony Beam!
    It shoots a laser!
    The laser detects enemies entering and leaving!
    It stops at walls!
    It reflects off of reflective objects and the reflective object behaves the same as the original laser!
    It doesnt hit the player!
    
    What cant it do?
    Make a sandwich.

--- Assets/Scripts/HarmonyBeam.cs
+++ Assets/Scripts/HarmonyBeam.cs
@@ -1,12 +1,17 @@
-using System.Collections;
+/******************************************************************
+*    Author: Claire Noto
+*    Contributors: Claire Noto
+*    Date Created: 10/10/24
+*    Description: Script that handles the harmony
+*******************************************************************/
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HarmonyBeam : MonoBehaviour
 {
-    [SerializeField] private LayerMask _layerMask;
-    [SerializeField] private float _laserDistance = 50f;
-    [SerializeField] private LineRenderer _lineRenderer;
+    private float _laserDistance = 50f;
+    private LineRenderer _lineRenderer;
+    private List<GameObject> _previouslyHitEnemies = new();
 
     void Start()
     {
@@ -15,32 +20,70 @@ public class HarmonyBeam : MonoBehaviour
 
     void FixedUpdate()
     {
-        ShootLaser();
+        ShootLaser(transform.position, transform.forward, _laserDistance);
     }
 
-    void ShootLaser()
+    /// <summary>
+    /// Shoots the line renderer and raycast.
+    /// </summary>
+    /// <param name="startPosition">position the laser starts from</param>
+    /// <param name="direction">direction the laser is going</param>
+    /// <param name="distance">distance the laser will go</param>
+    public void ShootLaser(Vector3 startPosition, Vector3 direction, float distance)
     {
-        _lineRenderer.SetPosition(0, transform.position);
+        _lineRenderer.positionCount = 2;  // Ensure the line renderer has 2 points (start and end)
+        _lineRenderer.SetPosition(0, startPosition);  // Starting point of the laser
 
-        RaycastHit[] hits = Physics.RaycastAll(transform.position, transform.forward, _laserDistance, _layerMask);
-        System.Array.Sort(hits, (hit1, hit2) => hit1.distance.CompareTo(hit2.distance));
-        Vector3 laserEndPoint = transform.position + transform.forward * _laserDistance;
+        RaycastHit[] hits = Physics.RaycastAll(startPosition, direction, distance);
+        System.Array.Sort(hits, (hit1, hit2) => hit1.distance.CompareTo(hit2.distance));  // Sort hits by distance
+
+        Vector3 laserEndPoint = startPosition + direction * distance;  // Default endpoint of the laser
+        List<GameObject> currentlyHitEnemies = new();
 
         foreach (RaycastHit hit in hits)
         {
-            switch (hit.collider.tag)
+            if (hit.collider.CompareTag("Enemy"))
             {
-                case "Enemy":
-                    Debug.Log("Hit an enemy: " + hit.collider.name);
-                    break;
-                case "Reflective":
-                    hit.collider.GetComponent<ReflectiveObject>().Reflect();
-                    break;
-            }
+                GameObject enemy = hit.collider.gameObject;
+                Debug.Log("Froze enemy");
 
-            laserEndPoint = hit.point;
+                // Add to the currently hit list
+                currentlyHitEnemies.Add(enemy);
+            }
+            else if (hit.collider.CompareTag("Reflective"))
+            {
+                // Reflect the laser using the ReflectiveObject's Reflect method
+                var reflectiveObject = hit.collider.GetComponent<ReflectiveObject>();
+                if (reflectiveObject)
+                {
+                    reflectiveObject.Reflect(direction, distance - hit.distance);
+                }
+
+                // Stop the main laser at the reflective object
+                laserEndPoint = hit.point;
+                break;
+            }
+            else
+            {
+                // Stop the laser at a non-enemy, non-reflective object
+                laserEndPoint = hit.point;
+                break;
+            }
         }
 
         _lineRenderer.SetPosition(1, laserEndPoint);
+
+        // Unfreeze any enemies that were previously hit but not hit now
+        foreach (GameObject enemy in _previouslyHitEnemies)
+        {
+            if (!currentlyHitEnemies.Contains(enemy))
+            {
+                Debug.Log("Unfroze enemy");
+            }
+        }
+
+        // Update the previously hit enemies list
+        _previouslyHitEnemies.Clear();
+        _previouslyHitEnemies.AddRange(currentlyHitEnemies);
     }
 }
--- Assets/Scripts/ReflectiveObject.cs
+++ Assets/Scripts/ReflectiveObject.cs
@@ -1,13 +1,81 @@
-using System.Collections;
-using System.Collections.Generic;
+/******************************************************************
+*    Author: Claire Noto
+*    Contributors: Claire Noto
+*    Date Created: 10/12/24
+*    Description: Script that handles harmony beam reflections
+*******************************************************************/
+
 using UnityEngine;
 
 public class ReflectiveObject : MonoBehaviour
 {
-    [SerializeField] private bool _left;
+    [SerializeField] private bool _reflectLeft = true;
 
-    public void Reflect()
+    private HarmonyBeam _harmonyBeam;
+    private LineRenderer _lineRenderer;
+    private bool _isHit = false;
+
+    void Start()
     {
+        _harmonyBeam = GetComponent<HarmonyBeam>();
+        _lineRenderer = GetComponent<LineRenderer>();
 
+        if (_harmonyBeam != null)
+        {
+            _harmonyBeam.enabled = false;
+        }
+
+        if (_lineRenderer != null)
+        {
+            _lineRenderer.enabled = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // If the object is not hit, disable the HarmonyBeam and LineRenderer
+        if (!_isHit && _harmonyBeam != null && _harmonyBeam.enabled)
+        {
+            _harmonyBeam.enabled = false;
+            if (_lineRenderer != null)
+            {
+                _lineRenderer.enabled = false;
+            }
+        }
+
+        _isHit = false;
+    }
+
+    /// <summary>
+    /// Reflects the harmony beam laser in the specified direction
+    /// </summary>
+    /// <param name="incomingDirection">direction the original laser is coming from</param>
+    /// <param name="remainingDistance">distance the beam can go</param>
+    public void Reflect(Vector3 incomingDirection, float remainingDistance)
+    {
+        _isHit = true;
+
+        if (_harmonyBeam != null && !_harmonyBeam.enabled)
+        {
+            Vector3 newDirection;
+            if (_reflectLeft)
+            {
+                newDirection = Quaternion.Euler(0, -90, 0) * incomingDirection;  // Rotate -90 degrees (left)
+            }
+            else
+            {
+                newDirection = Quaternion.Euler(0, 90, 0) * incomingDirection;   // Rotate 90 degrees (right)
+            }
+
+            transform.forward = newDirection;
+
+            _harmonyBeam.enabled = true;
+            if (_lineRenderer != null)
+            {
+                _lineRenderer.enabled = true;
+            }
+
+            _harmonyBeam.ShootLaser(transform.position, newDirection, remainingDistance);
+        }
     }
 }
