afebb89b860bef3cba7b17c94dcd58530488be20
NGrinstead0101 <ngrinstead01@gmail.com>
2024-10-12 13:41:11 -0500

    Set up Shader Graph for Screen Wipe

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,19 +1,22 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using NaughtyAttributes;
 
 public class SceneController : MonoBehaviour
 {
     public static SceneController Instance;
 
-    [SerializeField] private float _timeForSceneTransition;
-    [SerializeField] private Transform _circularWipeMaskTrans;
-    [SerializeField] private Transform _wipeBackgroundTrans;
+    [Required]
+    [SerializeField] private Image _circleWipeImage;
+    [SerializeField] private float _timeForScreenWipe;
 
-    private const float MaxWipeScale = 12.0f;
-    private const float MinWipeScale = 0.0f;
+    private readonly int _circleSizePropId = Shader.PropertyToID("_CircleSize");
 
+    /// <summary>
+    /// Creates instance and starts fade in transition
+    /// </summary>
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -22,38 +25,79 @@ public class SceneController : MonoBehaviour
         }
 
         Instance = this;
+
+        StartCoroutine(CircleWipeTransition(true));
     }
 
+    /// <summary>
+    /// Called to reload the current scene after a fade out transition
+    /// </summary>
     public void ReloadCurrentScene()
     {
-        
+        StopAllCoroutines();
+        string sceneName = SceneManager.GetActiveScene().name;
+        StartCoroutine(CircleWipeTransition(false, sceneName));
     }
 
+    /// <summary>
+    /// Overload for loading a new scene with build index
+    /// </summary>
+    /// <param name="sceneBuildIndex">Build index of the scene to load</param>
     public void LoadNewScene(int sceneBuildIndex)
     {
         LoadNewScene(SceneManager.GetSceneByBuildIndex(sceneBuildIndex).name);
     }
 
+    /// <summary>
+    /// Loads a new scene after a fade out transition
+    /// </summary>
+    /// <param name="sceneName">Name of the scene to load</param>
     public void LoadNewScene(string sceneName)
     {
-        StartCoroutine(nameof(DelayedSceneTransition));
-
-        //SceneManager.LoadScene(sceneName);
+        StopAllCoroutines();
+        StartCoroutine(CircleWipeTransition(false, sceneName));
     }
 
-    private IEnumerator DelayedSceneTransition()
+    /// <summary>
+    /// Handles the smoothly animating the circle wipe over a period of time
+    /// </summary>
+    /// <param name="isFadingIn">True if circle should move out, false if it should close in</param>
+    /// <param name="sceneToLoad">Name of the scene to load, only needed if fading out</param>
+    private IEnumerator CircleWipeTransition(bool isFadingIn, string sceneToLoad = "")
     {
         float elapsedTime = 0f;
         float lerpingTime;
-        Vector3 newScale = new Vector3();
+        float newCircleSize;
+        float startingCircleSize = isFadingIn ? 0f : 1f;
+        float targetCircleSize = isFadingIn ? 1f : 0f;
 
-        while (elapsedTime < _timeForSceneTransition)
+        _circleWipeImage.materialForRendering.SetFloat(_circleSizePropId, startingCircleSize);
+
+        // Animates circle wipe until the end time is reached
+        while (elapsedTime < _timeForScreenWipe)
         {
-            lerpingTime = elapsedTime / _timeForSceneTransition;
-            newScale.x = newScale.y = newScale.z = Mathf.Lerp(MaxWipeScale, MinWipeScale, lerpingTime);
-            _circularWipeMaskTrans.localScale = newScale;
+            lerpingTime = elapsedTime / _timeForScreenWipe;
+            newCircleSize = Mathf.Lerp(startingCircleSize, targetCircleSize, lerpingTime);
+            _circleWipeImage.materialForRendering.SetFloat(_circleSizePropId, newCircleSize);
             elapsedTime += Time.deltaTime;
             yield return null;
         }
+
+        _circleWipeImage.materialForRendering.SetFloat(_circleSizePropId, targetCircleSize);
+
+        // Loads new scene if needed
+        if (!isFadingIn && !sceneToLoad.Equals(""))
+            SceneManager.LoadScene(sceneToLoad);
     }
+
+#if UNITY_EDITOR
+    /// <summary>
+    /// Can be used for testing in editor
+    /// </summary>
+    [Button]
+    private void ReloadScene()
+    {
+        ReloadCurrentScene();
+    }
+#endif
 }
