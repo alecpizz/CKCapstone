bf46ab1452ab392d54d9fd875a9db6618af45292
NGrinstead0101 <ngrinstead01@gmail.com>
2024-10-24 11:28:23 -0500

    Integrated with player

--- Assets/Scripts/Entities/PlayerMovement.cs
+++ Assets/Scripts/Entities/PlayerMovement.cs
@@ -14,7 +14,7 @@ using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.Windows;
 
-public class PlayerMovement : MonoBehaviour, IGridEntry
+public class PlayerMovement : MonoBehaviour, IGridEntry, ITimeListener
 {
     private PlayerControls _input;
     public Vector3 FacingDirection { get; private set; }
@@ -28,12 +28,22 @@ public class PlayerMovement : MonoBehaviour, IGridEntry
     [SerializeField]
     private Vector3 _positionOffset;
 
+    private bool _playerMovementComplete = true;
+    private int _playerMovementTiming = 1;
+    private TimeSignatureManager _timeSigManager;
+
     // Start is called before the first frame update
     void Start()
     {
         FacingDirection = new Vector3(0, 0, 0);
 
         GridBase.Instance.AddEntry(this);
+
+        _timeSigManager = TimeSignatureManager.Instance;
+        if (_timeSigManager != null)
+        {
+            _timeSigManager.RegisterTimeListener(this);
+        }
 
         // Referencing and setup of the Input Action functions
         _input = new PlayerControls();
@@ -48,21 +58,52 @@ public class PlayerMovement : MonoBehaviour, IGridEntry
     {
         _input.InGame.Disable();
         _input.InGame.Movement.performed -= MovementPerformed;
+
+        if (_timeSigManager != null)
+        {
+            _timeSigManager.UnregisterTimeListener(this);
+        }
     }
 
+    /// <summary>
+    /// Moves the player to the next grid tile if able
+    /// </summary>
+    /// <param name="context">Input callback context</param>
     public void MovementPerformed(InputAction.CallbackContext context)
     {
-        Vector2 key = context.ReadValue<Vector2>();
-        Vector3 direction = new(key.x, 0, key.y);
-
-        // Move if there is no wall below the player or if ghost mode is enabled
-        var move = GridBase.Instance.GetCellPositionInDirection(gameObject.transform.position, direction);
-        if ((GridBase.Instance.CellIsEmpty(move) && enemiesMoved == true) ||
-            (DebugMenuManager.Instance.GhostMode && enemiesMoved == true))
+        if (_playerMovementComplete && enemiesMoved)
         {
-            gameObject.transform.position = move + _positionOffset;
-            GridBase.Instance.UpdateEntry(this);
+            _playerMovementComplete = false;
+
+            Vector2 key = context.ReadValue<Vector2>();
+            Vector3 direction = new(key.x, 0, key.y);
+
+            StartCoroutine(MovementDelay(direction));
         }
+    }
+
+    /// <summary>
+    /// Helper coroutine for performing movement with a delay
+    /// </summary>
+    /// <param name="moveDirection">Direction of player movement</param>
+    /// <returns>Waits for short delay while moving</returns>
+    private IEnumerator MovementDelay(Vector3 moveDirection)
+    {
+        for (int i = 0; i < _playerMovementTiming; i++)
+        {
+            // Move if there is no wall below the player or if ghost mode is enabled
+            var move = GridBase.Instance.GetCellPositionInDirection(gameObject.transform.position, moveDirection);
+            if ((GridBase.Instance.CellIsEmpty(move)) ||
+                (DebugMenuManager.Instance.GhostMode))
+            {
+                gameObject.transform.position = move + _positionOffset;
+                GridBase.Instance.UpdateEntry(this);
+            }
+
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        _playerMovementComplete = true;
     }
 
     /// <summary>
@@ -82,5 +123,17 @@ public class PlayerMovement : MonoBehaviour, IGridEntry
 
             SceneController.Instance.ReloadCurrentScene();
         }
+    }
+
+    /// <summary>
+    /// Receives the new player movement speed when time signature updates
+    /// </summary>
+    /// <param name="newTimeSignature">The new time signature</param>
+    public void UpdateTimingFromSignature(Vector2Int newTimeSignature)
+    {
+        _playerMovementTiming = newTimeSignature.x;
+
+        if (_playerMovementTiming <= 0)
+            _playerMovementTiming = 1;
     }
 }
