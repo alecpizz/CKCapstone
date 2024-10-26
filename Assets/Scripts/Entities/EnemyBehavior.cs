97050e01365e31b6c9c8a0b8f18d19ffc841d09b
Spygie <mjyoung@mail.bradley.edu>
2024-10-26 17:59:45 -0500

    It works
    The bug with players being able to double input still exists depending on wait time value

--- Assets/Scripts/Entities/EnemyBehavior.cs
+++ Assets/Scripts/Entities/EnemyBehavior.cs
@@ -32,7 +32,7 @@ public class EnemyBehavior : MonoBehaviour, IGridEntry, ITimeListener
     private PlayerMovement _playerMoveRef;
 
     //Wait time between enemy moving each individual tile while on path to next destination
-    [SerializeField] private float _waitTime = 0.05f;
+    [SerializeField] private float _waitTime = 0.5f;
 
     //List of movePoint structs that contain a direction enum and a tiles to move integer.
     public enum Direction { Up, Down, Left, Right }
@@ -51,6 +51,8 @@ public class EnemyBehavior : MonoBehaviour, IGridEntry, ITimeListener
     public bool enemyFrozen = false;
 
     private int _enemyMovementTime = 1;
+
+    [SerializeField] private float _tempMoveTime = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -163,7 +165,7 @@ public class EnemyBehavior : MonoBehaviour, IGridEntry, ITimeListener
                 /// </summary>
                 for (int j = 0; j < pointTiles; j++)
                 {
-                    var move = GridBase.Instance.GetCellPositionInDirection(gameObject.transform.position, 
+                    var move = GridBase.Instance.GetCellPositionInDirection(gameObject.transform.position,
                         moveInDirection);
                     var entries = GridBase.Instance.GetCellEntries(move);
                     bool breakLoop = false;
@@ -184,10 +186,15 @@ public class EnemyBehavior : MonoBehaviour, IGridEntry, ITimeListener
                         break;
                     }
 
-                    gameObject.transform.position = move + _positionOffset;
-                    GridBase.Instance.UpdateEntry(this);
+                    Vector3 startPos = transform.position;
 
-                    yield return new WaitForSeconds(_waitTime);
+                    for (float k = 0; k <= _tempMoveTime; k += Time.deltaTime)
+                    {
+                        transform.position = Vector3.Lerp(startPos, move + _positionOffset, k / _tempMoveTime);
+                        yield return null;
+                    }
+
+                    GridBase.Instance.UpdateEntry(this);
                 }
 
                 /// <summary>
