666531711daf9bee9ddd160ea16123735a5ab60b
MadisonGorman <104847801+MadisonGorman@users.noreply.github.com>
2024-11-07 11:43:01 -0600

    Began to Program the Cutscene Framework, particularly for the Challenge Levels
    Created a corresponding testing gym and script

---
+++ Assets/Scripts/CutsceneFramework.cs
@@ -0,0 +1,29 @@
+/******************************************************************
+*    Author: Madison Gorman
+*    Contributors: 
+*    Date Created: 11/07/24
+*    Description: Permits a cutscene (comprised of a static image, 
+*    subtitles, and audio) to play after the completion
+*    of a challenge room
+*******************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneFramework : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // SceneController.Instance.LoadNewScene()
+}
