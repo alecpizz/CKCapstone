21f406c1d79b5b73260f988009ebf3f8df34137e
MadisonGorman <104847801+MadisonGorman@users.noreply.github.com>
2024-11-09 22:36:35 -0600

    Continued Work on Establishing the Challenge Cutscene Framework

--- Assets/Scripts/CutsceneFramework.cs
+++ Assets/Scripts/CutsceneFramework.cs
@@ -4,26 +4,23 @@
 *    Date Created: 11/07/24
 *    Description: Permits a cutscene (comprised of a static image, 
 *    subtitles, and audio) to play after the completion
-*    of a challenge room
+*    of a challenge room.
 *******************************************************************/
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CutsceneFramework : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private int _loadingSceneIndex = 0;
+    [SerializeField] private GameObject _backgroundImage;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
-    }
 
-    // SceneController.Instance.LoadNewScene()
+    }
+    
+    // SceneController.Instance.LoadNewScene(_loadingSceneIndex);
 }
