d9b59c742b1c6723b9510cd95e3ad7d87e0bf1b5
Spygie <mjyoung@mail.bradley.edu>
2024-11-14 09:49:41 -0600

    Enemy Variant script consolidation
    Consolidated the enemy variants scripts to two scripts between the set path enemies and the player movement based enemies.

--- Assets/Scripts/Entities/MirrorAndCopyBehavior.cs
+++ Assets/Scripts/Entities/MirrorAndCopyBehavior.cs
@@ -2,8 +2,8 @@
 *    Author: Mitchell Young
 *    Contributors: Mitchell Young
 *    Date Created: 10/27/24
-*    Description: Script that handles the behavior of the mirror
-*    enemy that mirrors player movement.
+*    Description: Script that handles the behavior of the mirror and
+*    copy enemy that mirrors or copies player movement.
 *******************************************************************/
 
 using System.Collections;
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using PrimeTween;
 using UnityEngine;
 
-public class MirrorBehavior : MonoBehaviour, IGridEntry, ITimeListener, ITurnListener, IHarmonyBeamEntity
+public class MirrorAndCopyBehavior : MonoBehaviour, IGridEntry, ITimeListener, ITurnListener, IHarmonyBeamEntity
 {
     public bool IsTransparent { get => true; }
     public bool BlocksHarmonyBeam { get => false; }
@@ -26,9 +26,12 @@ public class MirrorBehavior : MonoBehaviour, IGridEntry, ITimeListener, ITurnLis
     private PlayerInteraction _playerInteraction;
     [SerializeField] private GameObject _player;
 
+    //Determines whether or not the enemy's movement is reversed
+    [SerializeField] private bool _mirrored;
+
     private float _movementTime = 0.55f;
 
-    private int _mirrorMovementTiming = 1;
+    private int _movementTiming = 1;
     private WaitForSeconds _waitForSeconds;
 
     // Start is called before the first frame update
@@ -59,52 +62,58 @@ public class MirrorBehavior : MonoBehaviour, IGridEntry, ITimeListener, ITurnLis
 
     private IEnumerator DelayedInput(Vector3 moveDirection)
     {
-        moveDirection = -moveDirection;
-
-        for (int i = 0; i < _mirrorMovementTiming; ++i)
+        if (!EnemyFrozen)
         {
-            // Moves if there is no objects in the next grid space
-            var move = GridBase.Instance.GetCellPositionInDirection(gameObject.transform.position, moveDirection);
-            var entries = GridBase.Instance.GetCellEntries(move);
-            bool canMove = true;
-
-            if (GridBase.Instance.CellIsEmpty(move))
+            if (_mirrored)
             {
-                //If the next cell contains an object that is not the player then the loop breaks
-                //enemy can't move into other enemies, walls, etc.
-                foreach (var entry in entries)
-                {
-                    if (entry.GetGameObject != _player)
-                    {
-                        canMove = false;
-                        break;
-                    }
-                }
-                if (canMove == true)
-                {
-                    yield return Tween.Position(transform,
-                        move + _positionOffset, _movementTime, ease: Ease.OutBack).OnUpdate<MirrorBehavior>(target: this, (target, tween) =>
-                        {
-                            GridBase.Instance.UpdateEntry(this);
-                        }).ToYieldInstruction();
-                }
-
-                GridBase.Instance.UpdateEntry(this);
+                moveDirection = -moveDirection;
             }
-            else
+
+            for (int i = 0; i < _movementTiming; ++i)
             {
-                if (_mirrorMovementTiming > 1)
+                // Moves if there is no objects in the next grid space
+                var move = GridBase.Instance.GetCellPositionInDirection(gameObject.transform.position, moveDirection);
+                var entries = GridBase.Instance.GetCellEntries(move);
+                bool canMove = true;
+
+                if (GridBase.Instance.CellIsEmpty(move))
+                {
+                    //If the next cell contains an object that is not the player then the loop breaks
+                    //enemy can't move into other enemies, walls, etc.
+                    foreach (var entry in entries)
+                    {
+                        if (entry.GetGameObject != _player)
+                        {
+                            canMove = false;
+                            break;
+                        }
+                    }
+                    if (canMove == true)
+                    {
+                        yield return Tween.Position(transform,
+                            move + _positionOffset, _movementTime, ease: Ease.OutBack).OnUpdate<MirrorAndCopyBehavior>(target: this, (target, tween) =>
+                            {
+                                GridBase.Instance.UpdateEntry(this);
+                            }).ToYieldInstruction();
+                    }
+
+                    GridBase.Instance.UpdateEntry(this);
+                }
+                else
+                {
+                    if (_movementTiming > 1)
+                    {
+                        yield return _waitForSeconds;
+                    }
+
+                    RoundManager.Instance.CompleteTurn(this);
+                    break;
+                }
+
+                if (_movementTiming > 1)
                 {
                     yield return _waitForSeconds;
                 }
-
-                RoundManager.Instance.CompleteTurn(this);
-                break;
-            }
-
-            if (_mirrorMovementTiming > 1)
-            {
-                yield return _waitForSeconds;
             }
         }
 
@@ -117,10 +126,10 @@ public class MirrorBehavior : MonoBehaviour, IGridEntry, ITimeListener, ITurnLis
         /// <param name="newTimeSignature">The new time signature</param>
         public void UpdateTimingFromSignature(Vector2Int newTimeSignature)
     {
-        _mirrorMovementTiming = newTimeSignature.x;
+        _movementTiming = newTimeSignature.x;
 
-        if (_mirrorMovementTiming <= 0)
-            _mirrorMovementTiming = 1;
+        if (_movementTiming <= 0)
+            _movementTiming = 1;
     }
     public TurnState TurnState => TurnState.World;
     public void BeginTurn(Vector3 direction)
