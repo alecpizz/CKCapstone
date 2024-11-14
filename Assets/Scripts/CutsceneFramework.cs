7179ea58d20480299fe0419d2948078318e75cab
MadisonGorman <104847801+MadisonGorman@users.noreply.github.com>
2024-11-13 22:07:21 -0600

    Added Video Playing Functionality to the Cutscene Framework Script

--- Assets/Scripts/CutsceneFramework.cs
+++ Assets/Scripts/CutsceneFramework.cs
@@ -2,18 +2,21 @@
 *    Author: Madison Gorman
 *    Contributors: 
 *    Date Created: 11/07/24
-*    Description: Permits two types of cutscenes to play, after the 
-*    completion of a challenge room (comprised of a static image, 
+*    Description: Permits one of two cutscene types to play; either after 
+*    the completion of a challenge level (comprised of a static image, 
 *    subtitles, and audio) and chapter (a video)
-*    Referenced https://www.youtube.com/watch?v=nt4qfbNAQqM to 
-*    implement the functionality surrounding playing a video for 
-*    the End Chapter Cutscene
+*    References: https://www.youtube.com/watch?v=nt4qfbNAQqM (Used 
+*    to implement the functionality for playing a video, particularly 
+*    for the End Chapter Cutscene)
 *******************************************************************/
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+// Referenced https://www.youtube.com/watch?v=nt4qfbNAQqM (Used to implement
+// the functionality for playing a video, particularly for the End Chapter Cutscene)
+using UnityEngine.Video; 
 
 public class CutsceneFramework : MonoBehaviour
 {
@@ -22,7 +25,12 @@ public class CutsceneFramework : MonoBehaviour
 
     [SerializeField] private List<GameObject> _challengeCutsceneImages;
 
-    //[SerializeField] private int _loadingSceneIndex = 0;
+    // Referenced https://www.youtube.com/watch?v=nt4qfbNAQqM (Used to implement
+    // the functionality for playing a video, particularly for the End Chapter Cutscene)
+    [SerializeField] private VideoPlayer _endChapterCutsceneVideo;
+    [SerializeField] private float _cutsceneDuration;
+
+    [SerializeField] private int _loadingLevelIndex = 0;
 
     private void Start()
     {
@@ -31,7 +39,7 @@ public class CutsceneFramework : MonoBehaviour
     
     private void PlayCutscene()
     {
-        if(_isChallengeCutscene == true && _isEndChapterCutscene == false)
+        if (_isChallengeCutscene == true && _isEndChapterCutscene == false)
         {
             foreach (GameObject image in _challengeCutsceneImages)
             {
@@ -41,9 +49,33 @@ public class CutsceneFramework : MonoBehaviour
 
         if (_isEndChapterCutscene == true && _isChallengeCutscene == false)
         {
-            
+            // Referenced https://www.youtube.com/watch?v=nt4qfbNAQqM (Used to implement the
+            // functionality for playing a video, particularly for the End Chapter Cutscene)
+            _endChapterCutsceneVideo.Play();
         }
 
-        //SceneController.Instance.LoadNewScene(_loadingSceneIndex);
+        // Referenced https://www.youtube.com/watch?v=nt4qfbNAQqM (Used to implement the
+        // functionality for playing a video, particularly for the End Chapter Cutscene)
+        StartCoroutine(CutsceneDuration());
+    }
+
+    // Referenced https://www.youtube.com/watch?v=nt4qfbNAQqM (Used to implement the
+    // functionality for playing a video, particularly for the End Chapter Cutscene)
+    private IEnumerator CutsceneDuration()
+    {
+        // Referenced https://www.youtube.com/watch?v=nt4qfbNAQqM (Used to implement the
+        // functionality for playing a video, particularly for the End Chapter Cutscene)
+        yield return new WaitForSeconds(_cutsceneDuration);
+
+        // Referenced https://www.youtube.com/watch?v=nt4qfbNAQqM (Used to implement the
+        // functionality for playing a video, particularly for the End Chapter Cutscene)
+        _endChapterCutsceneVideo.Stop();
+
+        foreach(GameObject image in _challengeCutsceneImages)
+        {
+            image.SetActive(false);
+        }
+
+        SceneController.Instance.LoadNewScene(_loadingLevelIndex);
     }
 }
