2b562a2fec2cc3b8eb003c30e73786b4d47a1be6
Trinity <thutson3876@gmail.com>
2024-11-19 19:43:43 -0600

    Added UIParticles to Metronome

--- Assets/Scripts/TimeSignature/MetronomeBehavior.cs
+++ Assets/Scripts/TimeSignature/MetronomeBehavior.cs
@@ -1,6 +1,6 @@
 /******************************************************************
 *    Author: Nick Grinstead
-*    Contributors: 
+*    Contributors: Trinity Hutson
 *    Date Created: 10/28/24
 *    Description: Checks for collisions with the player and then 
 *       calls the TimeSignatureManager to update the time signature.
@@ -8,9 +8,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MetronomeBehavior : MonoBehaviour
 {
+    public UnityEvent MetronomeTriggered;
+
     /// <summary>
     /// Toggles the time signature on the manager if there is one
     /// </summary>
@@ -18,6 +21,8 @@ public class MetronomeBehavior : MonoBehaviour
     {
         if (TimeSignatureManager.Instance != null)
             TimeSignatureManager.Instance.ToggleTimeSignature();
+
+        MetronomeTriggered?.Invoke();
     }
 
     /// <summary>
