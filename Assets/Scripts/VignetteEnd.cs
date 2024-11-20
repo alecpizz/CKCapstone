29a32352f672ba8d4905e04aedc770f1a6e8d471
NGrinstead0101 <ngrinstead01@gmail.com>
2024-11-19 21:08:36 -0600

    Comments

--- Assets/Scripts/VignetteEnd.cs
+++ Assets/Scripts/VignetteEnd.cs
@@ -1,9 +1,12 @@
+/******************************************************************
+ *    Author: Nick Grinstead
+ *    Contributors: 
+ *    Date Created: 11/19/24
+ *    Description: Script for playing the vignette fade out effect.
+ *      Assumes it's on the same object as a VignetteStart script.
+ *******************************************************************/
 using PrimeTween;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 public class VignetteEnd : MonoBehaviour, ITurnListener
@@ -16,7 +19,7 @@ public class VignetteEnd : MonoBehaviour, ITurnListener
     private float _vignetteIntensity;
 
     /// <summary>
-    /// Get vignette that VignetteStart set-up
+    /// Grab vignette that VignetteStart set-up
     /// </summary>
     private void Start()
     {
@@ -27,18 +30,29 @@ public class VignetteEnd : MonoBehaviour, ITurnListener
         RoundManager.Instance.RegisterListener(this);
     }
 
+    /// <summary>
+    /// Unregister from listener
+    /// </summary>
     private void OnDisable()
     {
         RoundManager.Instance.UnRegisterListener(this);
     }
 
+    /// <summary>
+    /// Tweens vignette intensity from _vignette intensity to 0.
+    /// </summary>
+    /// <param name="direction"></param>
     public void BeginTurn(Vector3 direction)
     {
         Tween.Custom(_vignetteIntensity, 0f, _vignetteFadeOutTime, newValue => _vignette.intensity.value = newValue)
             .OnComplete(() => RoundManager.Instance.CompleteTurn(this));
     }
 
+    /// <summary>
+    /// Completes turn early
+    /// </summary>
     public void ForceTurnEnd()
     {
+        RoundManager.Instance.CompleteTurn(this);
     }
 }
--- Assets/Scripts/VignetteStart.cs
+++ Assets/Scripts/VignetteStart.cs
@@ -1,7 +1,11 @@
+/******************************************************************
+ *    Author: Nick Grinstead
+ *    Contributors: 
+ *    Date Created: 11/19/24
+ *    Description: Script for playing the vignette fade in effect.
+ *      Assumes it's on the same object as a volume object.
+ *******************************************************************/
 using PrimeTween;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
@@ -30,23 +34,37 @@ public class VignetteStart : MonoBehaviour, ITurnListener
             _vignette.rounded.overrideState = true;
     }
 
+    /// <summary>
+    /// Register to round manager
+    /// </summary>
     private void Start()
     {
         RoundManager.Instance.RegisterListener(this);
     }
 
+    /// <summary>
+    /// Unregister from round manager
+    /// </summary>
     private void OnDisable()
     {
         RoundManager.Instance.UnRegisterListener(this);
     }
 
+    /// <summary>
+    /// Tweens vignette intensity from 0 to _vignetteIntensity
+    /// </summary>
+    /// <param name="direction"></param>
     public void BeginTurn(Vector3 direction)
     {
         Tween.Custom(0f, _vignetteIntensity, _vignetteFadeInTime, newValue => _vignette.intensity.value = newValue)
             .OnComplete(() => RoundManager.Instance.CompleteTurn(this));
     }
 
+    /// <summary>
+    /// Completes turn early
+    /// </summary>
     public void ForceTurnEnd()
     {
+        RoundManager.Instance.CompleteTurn(this);
     }
 }
