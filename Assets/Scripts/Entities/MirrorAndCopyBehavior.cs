426b3472fd543b34cd46c752a27f733031dc085f
Trinity <thutson3876@gmail.com>
2024-11-25 18:00:16 -0600

    Fixed Round timing Bug

--- Assets/Scripts/Entities/MirrorAndCopyBehavior.cs
+++ Assets/Scripts/Entities/MirrorAndCopyBehavior.cs
@@ -166,7 +166,7 @@ public class MirrorAndCopyBehavior : MonoBehaviour, IGridEntry, ITimeListener, I
             SceneController.Instance.ReloadCurrentScene();
         }
     }
-    public TurnState TurnState => TurnState.Player;
+    public TurnState TurnState => TurnState.Enemy;
 
     public void BeginTurn(Vector3 direction)
     {
