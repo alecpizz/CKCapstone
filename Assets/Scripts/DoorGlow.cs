fc99e50c6919df0af77e5b7e71f6a653120f909f
Josie <jqualls@mail.bradley.edu>
2025-01-23 10:17:33 -0600

    Fixed safety infractions on Taylor's DoorGlow script

--- Assets/Scripts/DoorGlow.cs
+++ Assets/Scripts/DoorGlow.cs
@@ -2,7 +2,8 @@
 *    Author: Taylor Sims
 *    Contributors: Trinity
 *    Date Created: 10/22/24
-*    Description: 
+*    Description: Creates a glow around the door.
+*    Changes the door visuals once the door is unlocked and opened.
 *******************************************************************/
 using System.Collections;
 using System.Collections.Generic;
@@ -10,17 +11,22 @@ using UnityEngine;
 
 public class DoorGlow : MonoBehaviour
 {
-    [SerializeField] private Color glowColor = Color.yellow; // Set the glow color
-    [SerializeField] private float glowIntensity = 15.0f; // Intensity of the glow
+    [SerializeField] private Color _glowColor = Color.yellow; // Set the glow color
+    [SerializeField] private float _glowIntensity = 15.0f; // Intensity of the glow
 
     private Renderer _renderer; // The renderer attached to this object
 
+    /// <summary>
+    /// Renders the glow on the door once the function wakes
+    /// </summary>
     private void Awake()
     {
         _renderer = GetComponent<Renderer>();
     }
 
-    // Combined method to trigger door unlocking and visuals
+    /// <summary>
+    /// Combined method to trigger door unlocking and visuals
+    /// </summary>
     public void GlowAndUnlockDoor()
     {
         if (_renderer == null)
@@ -30,6 +36,6 @@ public class DoorGlow : MonoBehaviour
 
         // Enable the emission and set the color to make the door glow
         _renderer.material.EnableKeyword("_EMISSION");
-        _renderer.material.SetColor("_EmissionColor", glowColor * glowIntensity);
+        _renderer.material.SetColor("_EmissionColor", _glowColor * _glowIntensity);
     }
 }
