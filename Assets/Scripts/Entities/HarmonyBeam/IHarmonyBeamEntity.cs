de2ee1c167f9de16362fed47c60bf2040c148894
NGrinstead0101 <ngrinstead01@gmail.com>
2025-01-26 10:15:07 -0600

    Adjusted Spacing

--- Assets/Scripts/Entities/HarmonyBeam/IHarmonyBeamEntity.cs
+++ Assets/Scripts/Entities/HarmonyBeam/IHarmonyBeamEntity.cs
@@ -17,20 +17,23 @@ public interface IHarmonyBeamEntity
     /// Whether lasers can pass through this object.
     /// </summary>
     public bool AllowLaserPassThrough { get; }
+
     /// <summary>
     /// When a laser hit this object.
-    /// </summary>
-    
+    /// </summary> 
     public void OnLaserHit();
+
     /// <summary>
     /// When a laser left this object after
     /// previously hitting it.
     /// </summary>
     public void OnLaserExit();
+
     /// <summary>
     /// Whether the laser should spawn vfx to wrap around this object.
     /// </summary>
     public bool HitWrapAround { get; }
+
     /// <summary>
     /// The position of the object.
     /// </summary>
