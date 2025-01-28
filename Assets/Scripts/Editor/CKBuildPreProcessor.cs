cf807afcc3a709f189ebdfb2ca8e172e9aa6de0f
Alec Pizziferro <alecpizz@gmail.com>
2025-01-27 19:01:16 -0600

    chapter schema

---
+++ Assets/Scripts/Editor/CKBuildPreProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class CKBuildPreProcessor : IPreprocessBuildWithReport
+{
+    public int callbackOrder => 0;
+
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        Debug.Log($"Starting Build for platform {report.summary.platform}");
+        
+        //apply build scenes
+        BuildSceneIndex();
+    }
+
+    
+    [MenuItem("Tools/Crowded Kitchen/Preview Build Order")]
+    public static void BuildSceneIndex()
+    {
+        // // Find valid Scene paths and make a list of EditorBuildSettingsScene
+        List<EditorBuildSettingsScene> editorBuildSettingsScenes = new List<EditorBuildSettingsScene>();
+        
+        foreach (var data in LevelOrder.instance.Levels)
+        {
+            var sceneAsset = data.Scene;
+            editorBuildSettingsScenes.Add(new EditorBuildSettingsScene(AssetDatabase.GetAssetPath(sceneAsset), true));
+        }
+        
+        // Set the active platform or build profile scene list
+        EditorBuildSettings.scenes = editorBuildSettingsScenes.ToArray();
+    }
+}
--- Assets/Scripts/Editor/LevelOrder.cs
+++ Assets/Scripts/Editor/LevelOrder.cs
@@ -1,28 +1,65 @@
+using System;
 using System.Collections.Generic;
+using SaintsField;
 using SaintsField.Playa;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 [CreateAssetMenu(fileName = "LevelOrder", menuName = "ScriptableObjects/Level Order", order = 0)]
 public class LevelOrder : ScriptableSingleton<LevelOrder>
 {
-    [field: SerializeField] public List<SceneAsset> Scenes { get; private set; } = new List<SceneAsset>();
-
-
-    [Button]
-    private void PopulateBuildOrder()
+    [Serializable]
+    public class Chapter
     {
-        // Find valid Scene paths and make a list of EditorBuildSettingsScene
-        List<EditorBuildSettingsScene> editorBuildSettingsScenes = new List<EditorBuildSettingsScene>();
-        foreach (var sceneAsset in Scenes)
-        {
-            string scenePath = AssetDatabase.GetAssetPath(sceneAsset);
-            if (!string.IsNullOrEmpty(scenePath))
-                editorBuildSettingsScenes.Add(new EditorBuildSettingsScene(scenePath, true));
-        }
+        [field: SerializeField] public string ChapterName { get; private set; } = "Chapter CHANGEME";
+        [field: SerializeField, SepTitle("Intro Level", EColor.Green), SaintsRow(inline:true)] public LevelData Intro { get; private set; } = new();
+        [field: SerializeField, SepTitle("Puzzles", EColor.Yellow)] public List<LevelData> Levels { get; private set; } = new();
+        [field: SerializeField, SepTitle("Outro Level", EColor.Red), SaintsRow(inline:true)] public LevelData Outro { get; private set; } = new();
+    }
+    [Serializable]
+    public class LevelData
+    {
+        [field: SerializeField] public string LevelName { get; private set; } = "CHANGEME";
+        [field: SerializeField] public SceneAsset Scene { get; private set; }
+        [field: SerializeField] public bool UseNextLevelInListAsExit { get; private set; } = true;
 
-        // Set the active platform or build profile scene list
-        EditorBuildSettings.scenes = editorBuildSettingsScenes.ToArray();
+        [field: SerializeField, HideIf(nameof(UseNextLevelInListAsExit))]
+        public SceneAsset ExitScene { get; private set; }
+
+        [field: SerializeField] public bool HasChallengeExit { get; private set; } = false;
+
+        [field: SerializeField, ShowIf(nameof(HasChallengeExit))]
+        public SceneAsset ChallengeScene { get; private set; }
+    }
+
+    [field: SerializeField]
+    public List<LevelData> Levels { get; private set; } = new();
+    [field: SerializeField]
+    public List<Chapter> Chapters { get; private set; } = new();
+
+    [SepTitle("Level Input", EColor.White)] [SerializeField] 
+    [BelowButton(nameof(AddLevel), groupBy: "Build")]
+    [BelowButton(nameof(RunBuildPreProcess), groupBy: "Build")]
+    [BelowButton(nameof(ClearLevels), groupBy: "Build")]
+    [SaintsRow(inline:true)]
+    private LevelData _inputLevelData;
+
+    
+    private void AddLevel()
+    {
+        Levels.Add(_inputLevelData);
+        EditorUtility.SetDirty(this);
+    }
+    
+    private void RunBuildPreProcess()
+    {
+        CKBuildPreProcessor.BuildSceneIndex();
+    }
+
+    private void ClearLevels()
+    {
+        Levels.Clear();
+        EditorUtility.SetDirty(this);
+        EditorUtility.SetDirty(EditorWindow.focusedWindow);
     }
 }
