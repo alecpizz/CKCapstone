1443cf15b612eb01895ca94e2468cf2f352ed791
Josie <144286809+jjqualls@users.noreply.github.com>
2025-02-11 11:04:49 -0600

    Hard coding levels to tabs

--- Assets/Scripts/Buttons for Levels/LevelButtons.cs
+++ Assets/Scripts/Buttons for Levels/LevelButtons.cs
@@ -7,6 +7,7 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -16,19 +17,30 @@ public class LevelButtons : MonoBehaviour
 {
     //Button in charge of levels
     [SerializeField] private GameObject _buttonPrefab;
+    [SerializeField] private int _numLevels = 11;
+
     
     /// <summary>
     /// At run-time, buttons equal to the number of scenes are generated.
     /// </summary>
     private void Start()
     {
+        int tally = 1;
+        int lvlIncrease = 10;
+        int range = _numLevels;
+
         //cycles through all loaded scenes except main menu
-        for (int i = 1; i < SceneNum(); i++)
+        for (int i = 1; i <= 5; i++)
         {
-            //loads scene when button is clicked
-            IndividualButtons obj = Instantiate(_buttonPrefab, transform).GetComponent<IndividualButtons>();
-            obj.GetComponentInChildren<TextMeshProUGUI>().text = "Level " + i;
-            obj.SetIndex(i);
+            for (; tally < range; tally++)
+            {
+                //loads scene when button is clicked
+                IndividualButtons obj = Instantiate(_buttonPrefab, transform).GetComponent<IndividualButtons>();
+                obj.GetComponentInChildren<TextMeshProUGUI>().text = "Level " + tally;
+                obj.SetIndex(tally);
+                Debug.Log(tally);
+            }
+            range += lvlIncrease;
         }
     }
 
