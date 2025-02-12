2443ce84a960e2bfa4fc0e0ff78dd39c4da5d4e5
Josie <144286809+jjqualls@users.noreply.github.com>
2025-02-11 22:39:03 -0600

    Buttons are created and formatted under chapter buttons
    In the level select, you can click on a chapter button and 10 lvls per chapter are shown. Right now only 6 work in build cause there are only 6 scenes in build. The levels will need to be listed in order of chapter when going into the build.

--- Assets/Scripts/Buttons for Levels/LevelButtons.cs
+++ Assets/Scripts/Buttons for Levels/LevelButtons.cs
@@ -1,6 +1,6 @@
 /******************************************************************
 *    Author: Josephine Qualls
-*    Contributors: Trinity Hutson
+*    Contributors: Trinity Hutson, Alex Laubenstein
 *    Date Created: 01/28/2025
 *    Description: Generates buttons for every level in the game.
 *******************************************************************/
@@ -17,30 +17,71 @@ public class LevelButtons : MonoBehaviour
 {
     //Button in charge of levels
     [SerializeField] private GameObject _buttonPrefab;
-    [SerializeField] private int _numLevels = 11;
 
-    
+    //the number of levels that a chapter contains
+    [SerializeField] private int _numLevels = 10;
+
+    //List of chapters in game
+    [SerializeField] private List<GameObject> _levels;
+
+    //buttons to each chapter in game
+    [SerializeField] private Button _lvl1Button;
+    [SerializeField] private Button _lvl2Button;
+    [SerializeField] private Button _lvl3Button;
+    [SerializeField] private Button _lvl4Button;
+    [SerializeField] private Button _lvl5Button;
+
     /// <summary>
     /// At run-time, buttons equal to the number of scenes are generated.
     /// </summary>
     private void Start()
     {
+        //variables to help keep the for loops on track
         int tally = 1;
         int lvlIncrease = 10;
-        int range = _numLevels;
+        int range = _numLevels + 1;
 
-        //cycles through all loaded scenes except main menu
+        //cycles through all chapters and assigns levels (if they exist in build)
         for (int i = 1; i <= 5; i++)
         {
+            //initially sets the gameObjects to off
+            _levels[i-1].SetActive(false);
+
             for (; tally < range; tally++)
             {
                 //loads scene when button is clicked
-                IndividualButtons obj = Instantiate(_buttonPrefab, transform).GetComponent<IndividualButtons>();
+                IndividualButtons obj = Instantiate(_buttonPrefab, _levels[i-1].transform).GetComponent<IndividualButtons>();
                 obj.GetComponentInChildren<TextMeshProUGUI>().text = "Level " + tally;
                 obj.SetIndex(tally);
-                Debug.Log(tally);
             }
             range += lvlIncrease;
+        }
+
+        //buttons change their respective GameObjects
+        _lvl1Button.onClick.AddListener(() => ActivateButton(0));
+        _lvl2Button.onClick.AddListener(() => ActivateButton(1));
+        _lvl3Button.onClick.AddListener(() => ActivateButton(2));
+        _lvl4Button.onClick.AddListener(() => ActivateButton(3));
+        _lvl5Button.onClick.AddListener(() => ActivateButton(4));
+    }
+
+    /// <summary>
+    /// Sets a game object to true if the button is pressed, and everything else stays false.
+    /// </summary>
+    /// <param name="num"></param>
+    private void ActivateButton(int num)
+    {
+        
+        foreach(GameObject obj in _levels)
+        {
+            if(obj != _levels[num])
+            {
+                obj.SetActive(false);
+            }
+            else
+            {
+                obj.SetActive(true);
+            }
         }
     }
 
