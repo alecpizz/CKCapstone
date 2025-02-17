89ffad727b8c2329ea82e1e502a73905d546de92
Alec Pizziferro <alecpizz@gmail.com>
2025-02-17 14:21:23 -0600

    some comments

--- Assets/Scripts/Utils/LevelOrderSelection.cs
+++ Assets/Scripts/Utils/LevelOrderSelection.cs
@@ -1,3 +1,10 @@
+/******************************************************************
+ *    Author: Alec Pizziferro
+ *    Contributors:  nullptr
+ *    Date Created: 2/17/2025
+ *    Description: A scriptable object singleton to hold the
+ *    selected level order in build.
+ *******************************************************************/
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "LevelOrderSelection", menuName = "ScriptableObjects/Level Order Singleton", order = 0)]
