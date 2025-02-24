1fa789c3ba38501676ebf3628019d8522e63c646
Alec Pizziferro <alecpizz@gmail.com>
2025-02-24 17:47:17 -0600

    Update headers.

--- Assets/SaveDataManager.cs
+++ Assets/SaveDataManager.cs
@@ -1,3 +1,10 @@
+/******************************************************************
+ *    Author: Alec Pizziferro
+ *    Contributors: nullptr
+ *    Date Created: 2/24/2025
+ *    Description: Singleton instance/Abstraction for interacting with
+ *    DataBox save data.
+ *******************************************************************/
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -10,6 +17,9 @@ public class SaveDataManager : MonoBehaviour
    private static SaveDataManager _instance;
    public static Databox.DataboxObject MainSaveData => _instance._dataObject;
 
+   /// <summary>
+   /// Initialize the singleton.
+   /// </summary>
    private void Awake()
    {
       _instance = this;
