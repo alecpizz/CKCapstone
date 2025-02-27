61cff225ca8bc719ca2df3e0289f27469a1cfaa7
Josie <144286809+jjqualls@users.noreply.github.com>
2025-02-27 11:49:48 -0600

    Finally found where to change Level names

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -93,7 +93,7 @@ public class UIManager : MonoBehaviour, ITimeListener
         {
             int index = SceneManager.GetActiveScene().buildIndex;
             var prettyName = LevelOrderSelection.Instance.SelectedLevelData.PrettySceneNames[index].PrettyName;
-            _levelNumber.text = prettyName;
+            _levelNumber.text = "Level " + index;
         }
         else
         {
