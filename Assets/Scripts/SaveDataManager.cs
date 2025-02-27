2817d46d8564df840056f2276ccc08ab23a3db76
Alec Pizziferro <alecpizz@gmail.com>
2025-02-27 11:17:53 -0600

    persistent data, fuck player prefs lol

--- Assets/Scripts/SaveDataManager.cs
+++ Assets/Scripts/SaveDataManager.cs
@@ -8,6 +8,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using Databox;
 using UnityEngine;
 
@@ -22,19 +23,30 @@ public enum DataType
 public static class SaveDataManager
 {
     private static DataboxObject _saveDataReference = null;
+    private static DataboxObject _persistentDataObject = null;
     private const string SettingsTableName = "Settings";
     private const string ProgressionTableName = "Progression";
+    private static readonly string PersistentDataPath = Path.Combine(Application.persistentDataPath, "CKSave.json");
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Init()
     {
-        if (_saveDataReference == null)
+        if (_saveDataReference == null || _persistentDataObject == null)
         {
-            _saveDataReference = Resources.FindObjectsOfTypeAll<DataboxObject>()[0];
+            _saveDataReference  = Resources.FindObjectsOfTypeAll<DataboxObject>()[0];
             _saveDataReference.LoadDatabase();
+            _persistentDataObject = ScriptableObject.CreateInstance<DataboxObject>();
+
+            if (!File.Exists(PersistentDataPath))
+            {
+                var referenceDataJson = Resources.Load<TextAsset>("CKSave");
+                File.WriteAllText(Path.Combine(Application.persistentDataPath, "CKSave.json"), 
+                referenceDataJson.text);
+            }
+            _persistentDataObject.LoadDatabase(PersistentDataPath);
         }
     }
-    public static Databox.DataboxObject MainSaveData => _saveDataReference;
+    public static Databox.DataboxObject MainSaveData => _persistentDataObject;
 
     public static void SaveSetting(DataboxType dataBoxType, string entry, string value)
     {
@@ -43,12 +55,13 @@ public static class SaveDataManager
 
     public static void SetDirty()
     {
-        _saveDataReference.SaveDatabase();
+        _persistentDataObject.SaveDatabase(PersistentDataPath);
+        Debug.Log(_persistentDataObject.errors.ToString());
     }
 
     public static void ResetSave()
     {
-        _saveDataReference.ResetToInitValues(SettingsTableName);
-        _saveDataReference.ResetToInitValues(ProgressionTableName);
+        _persistentDataObject.ResetToInitValues(SettingsTableName);
+        _persistentDataObject.ResetToInitValues(ProgressionTableName);
     }
 }
