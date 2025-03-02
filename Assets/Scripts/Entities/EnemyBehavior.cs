65f1c1afb4fd4c558950f36ef9f130988829b03b
EeveeonGamer27 <101752853+EeveeonGamer27@users.noreply.github.com>
2025-03-02 14:22:07 -0600

    Son Freezes

--- Assets/Scripts/Entities/EnemyBehavior.cs
+++ Assets/Scripts/Entities/EnemyBehavior.cs
@@ -141,6 +141,7 @@ public class EnemyBehavior : MonoBehaviour, IGridEntry, ITimeListener,
 
     //public static PlayerMovement Instance;
     private static readonly int Forward = Animator.StringToHash("Forward");
+    private static readonly int Frozen = Animator.StringToHash("Frozen");
 
 
     [SerializeField] private Animator _animator;
@@ -422,6 +423,7 @@ public class EnemyBehavior : MonoBehaviour, IGridEntry, ITimeListener,
 
             if (_animator != null)
             {
+                _animator.SetBool(Frozen, false);
                 _animator.SetTrigger(Forward);
             }
             var dist = Vector3Int.Distance(currCell, goalCell);
@@ -590,6 +592,10 @@ public class EnemyBehavior : MonoBehaviour, IGridEntry, ITimeListener,
     {
         if (_isSonEnemy)
         {
+            if (_animator != null)
+            {
+                _animator.SetBool(Frozen, true);
+            }
             _isFrozen = true;
         }
     }
@@ -599,6 +605,10 @@ public class EnemyBehavior : MonoBehaviour, IGridEntry, ITimeListener,
     /// </summary>
     public void OnLaserExit()
     {
+        if (_animator != null)
+        {
+            _animator.SetBool(Frozen, false);
+        }
         _isFrozen = false;
     }
 
