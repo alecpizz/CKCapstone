15bb18fc3ecfc0b2b64c4a6d3234f0d907bf9d8b
AlecatEnjoyer <Mark25608@yahoo.com>
2025-03-04 09:57:08 -0600

    Moving wall sfx

--- Assets/Scripts/WallMovement/MovingWall.cs
+++ Assets/Scripts/WallMovement/MovingWall.cs
@@ -9,6 +9,7 @@ using PrimeTween;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
+using FMODUnity;
 using UnityEditor;
 using UnityEngine;
 using SaintsField.Playa;
@@ -61,6 +62,8 @@ public class MovingWall : MonoBehaviour, IParentSwitch, IGridEntry
 
     public GameObject EntryObject => gameObject;
 
+    [SerializeField] private EventReference _wallSound = default;
+
     public Vector3 Position => transform.position;
 
     private bool _shouldActivate = false;
@@ -100,6 +103,7 @@ public class MovingWall : MonoBehaviour, IParentSwitch, IGridEntry
     public void SwitchActivation()
     {
         _shouldActivate = !_shouldActivate;
+        AudioManager.Instance.PlaySound(_wallSound);
         MoveObject();
     }
 
