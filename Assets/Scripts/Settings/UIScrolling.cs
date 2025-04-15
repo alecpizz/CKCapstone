813a9488331f8e11c61aa476c02c2285e61d0bea
NGrinstead0101 <ngrinstead01@gmail.com>
2025-04-15 09:33:59 -0500

    Comments and Small Change

--- Assets/Scripts/Settings/UIScrolling.cs
+++ Assets/Scripts/Settings/UIScrolling.cs
@@ -1,3 +1,10 @@
+/******************************************************************
+*    Author: Nick Grinstead
+*    Contributors: 
+*    Date Created: April 14th, 2025
+*    Description: This script adds functionality to dropdowns to
+*       allow for scrolling with the mouse wheel.
+*******************************************************************/
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -10,10 +17,11 @@ public class UIScrolling : MonoBehaviour
     [SerializeField] private ScrollRect _scrollRect;
     [SerializeField] private float _scrollSensitivity = 0.1f;
 
-    private bool _isHovered = false;
-
     private DebugInputActions _inputActions;
 
+    /// <summary>
+    /// Initializing input action
+    /// </summary>
     private void OnEnable()
     {
         _inputActions = new DebugInputActions();
@@ -21,28 +29,23 @@ public class UIScrolling : MonoBehaviour
         _inputActions.UI.Scroll.performed += OnScrollInput;
     }
 
+    /// <summary>
+    /// Disabling input action
+    /// </summary>
     private void OnDisable()
     {
         _inputActions.UI.Disable();
         _inputActions.UI.Scroll.performed -= OnScrollInput;
     }
 
-    private void OnMouseEnter()
-    {
-        _isHovered = true;
-    }
-
-    private void OnMouseExit()
-    {
-        _isHovered = false;
-    }
-
+    /// <summary>
+    /// Moves the position of the scroll rect
+    /// </summary>
     private void OnScrollInput(InputAction.CallbackContext context)
     {
         Vector2 newPosition = _scrollRect.normalizedPosition;
         float changeInScroll = context.ReadValue<float>();
         newPosition.y = (int)changeInScroll;
         _scrollRect.normalizedPosition += newPosition.normalized * _scrollSensitivity;
-
     }
 }
