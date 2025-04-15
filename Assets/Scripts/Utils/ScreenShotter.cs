e5720f31ce992e911f85d340f212c69717ffd5d8
Alec Pizziferro <alecpizz@gmail.com>
2025-04-15 09:41:58 -0500

    Add comments

--- Assets/Scripts/Utils/ScreenShotter.cs
+++ Assets/Scripts/Utils/ScreenShotter.cs
@@ -1,3 +1,9 @@
+/******************************************************************
+*    Author: Alec Pizziferro
+*    Contributors: nullptr
+*    Date Created: 4/15/2025
+*    Description: Tool to capture screenshots. Hit G!
+*******************************************************************/
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,17 +13,18 @@ using UnityEngine.InputSystem;
 public class ScreenShotter : MonoBehaviour
 {
     [SerializeField] private int _captureScale = 4;
-
+    [SerializeField] private Key _captureKey = Key.G;
+    /// <summary>
+    /// Poll keyboard inputs and capture a screenshot.
+    /// Doesn't use input system as this should be unusued in a build.
+    /// </summary>
     private void Update()
     {
-        if (Keyboard.current[Key.G].wasPressedThisFrame)
-        {
-            string captureName = "Screenshot_" +
-                                 System.DateTime.Now.ToString(
-                                     "dd-MM-yyyy-HH-mm-ss") +
-                                 ".png";
-            ScreenCapture.CaptureScreenshot(captureName, _captureScale);
-            Debug.Log("Captured");
-        }
+        if (!Keyboard.current[_captureKey].wasPressedThisFrame) return;
+        string captureName = "Screenshot_" +
+                             System.DateTime.Now.ToString(
+                                 "dd-MM-yyyy-HH-mm-ss") +
+                             ".png";
+        ScreenCapture.CaptureScreenshot(captureName, _captureScale);
     }
 }
