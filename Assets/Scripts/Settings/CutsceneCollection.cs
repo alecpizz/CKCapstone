89ba6cfb7b354ea96d873e308fe099e2bc8e054a
Josie <144286809+jjqualls@users.noreply.github.com>
2025-05-01 11:20:14 -0500

    Finished making the hover text for cutscenes work
    Currently Cutscene 16 doesn't lead anywhere and therefore doesn't have a name because it doesn't exist in Level Order.

--- Assets/Scripts/Settings/CutsceneCollection.cs
+++ Assets/Scripts/Settings/CutsceneCollection.cs
@@ -1,6 +1,6 @@
 /******************************************************************
 *    Author: Mitchell Young
-*    Contributors: 
+*    Contributors: Josephine Qualls
 *    Date Created: 3/25/25
 *    Description: Loads cutscenes based on button pressed. Button
 *    cannot be pressed if the cutscene has not been seen before.
@@ -10,11 +10,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.IO;
+using UnityEngine.EventSystems;
+using TMPro;
 
-public class CutsceneCollection : MonoBehaviour
+public class CutsceneCollection : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private bool unlocked = false;
     [SerializeField] private string cutsceneName = "";
+    [SerializeField] private GameObject _nameOfCutscene;
 
     /// <summary>
     /// Sets unlocked to true if the cutscene has been seen
@@ -33,7 +37,49 @@ public class CutsceneCollection : MonoBehaviour
         {
             gameObject.GetComponent<Image>().color = new Color(0, 0, 0, 1);
         }
+        
+        //For the beginning of the hover text behavior
+        TextMeshProUGUI cutsceneNameText = _nameOfCutscene.GetComponent<TextMeshProUGUI>();
+        cutsceneNameText.text = LevelName();
 
+        if (_nameOfCutscene != null)
+        {
+            _nameOfCutscene.SetActive(false);
+        }
+
+    }
+
+    /// <summary>
+    /// Provides the correct name for the cutscenes
+    /// </summary>
+    /// <returns></returns>
+    private string LevelName()
+    {
+        var levelData = LevelOrderSelection.Instance.SelectedLevelData;
+
+        string name = "";
+
+        foreach (var levelDataChapter in levelData.Chapters)
+        {
+            foreach (var level in levelDataChapter.Puzzles)
+            {
+                var sceneName = Path.GetFileNameWithoutExtension(level.ScenePath);
+                if (sceneName.Equals(cutsceneName))
+                {
+                    name = level.LevelName;
+                }
+            }
+
+            var outroName = Path.GetFileNameWithoutExtension(levelDataChapter.Outro.ScenePath);
+            if (outroName.Equals(cutsceneName))
+            {
+                name = levelDataChapter.Outro.LevelName;
+                
+            }
+
+        }
+
+        return name;
     }
 
     /// <summary>
@@ -49,6 +95,30 @@ public class CutsceneCollection : MonoBehaviour
         SaveDataManager.SetSceneLoadedFrom(SceneManager.GetActiveScene().name);
 
         SceneManager.LoadScene(cutsceneName);
+    }
+
+    /// <summary>
+    /// Activates the hover text
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (_nameOfCutscene != null)
+        {
+            _nameOfCutscene.SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// Deactivates the hover text
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (_nameOfCutscene != null)
+        {
+            _nameOfCutscene.SetActive(false);
+        }
     }
 
 }
