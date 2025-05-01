03a6a5a31c4c64771d92d76f275b1cdefdab6078
Spygie <mjyoung@mail.bradley.edu>
2025-05-01 09:23:28 -0500

    Check for circular

--- Assets/Scripts/Entities/EnemyBehavior.cs
+++ Assets/Scripts/Entities/EnemyBehavior.cs
@@ -816,7 +816,7 @@ public class EnemyBehavior : MonoBehaviour, IGridEntry, ITimeListener,
         if (_waitOnBeam)
             _waitOnBeam = false;
 
-        if (_moveIndex == 0 || _moveIndex == _moveDestinations.Count - 1)
+        if (_moveIndex == 0 && !_circularMovement || _moveIndex == _moveDestinations.Count - 1 && !_circularMovement)
         {
             /*if (_animator != null)
             {
