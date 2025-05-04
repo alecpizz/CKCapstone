5e1fe360751e0915d4eab5e0fdb0e4bf82d5d54b
Trinity <thutson3876@gmail.com>
2025-05-04 15:42:40 -0500

    Implemented new Scrolling

---
+++ Assets/Scripts/Settings/CreditsScroll.cs
@@ -0,0 +1,71 @@
+/******************************************************************
+*    Author: Trinity Hutson
+*    Contributors: 
+*    Date Created: 5/4/2025
+*    Description: Scrolls a canvas component for rolling credits
+*******************************************************************/
+using PrimeTween;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem.Utilities;
+
+public class CreditsScroll : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform _creditsTransform;
+    [Space]
+    [SerializeField, Range(1, 100)]
+    float _scrollSpeed = 50;
+    [SerializeField]
+    float _skipSpeedMult = 5;
+    [Space]
+    [SerializeField]
+    float _creditsLength = 1000;
+
+    float _scrollDuration;
+
+    Tween _scrollTween;
+
+    private void Awake()
+    {
+        _scrollDuration = 1000 / _scrollSpeed;
+    }
+
+    private void Start()
+    {
+        ScrollCredits();
+    }
+
+    private void Update()
+    {
+        if (!_scrollTween.isAlive)
+            return;
+
+        foreach (var device in InputSystem.devices)
+        {
+            foreach (var control in device.allControls)
+            {
+                if (control is ButtonControl button && button.isPressed)
+                {
+                    _scrollTween.timeScale = _skipSpeedMult;
+                    return;
+                }
+            }
+        }
+
+        _scrollTween.timeScale = 1;
+    }
+
+    private void ScrollCredits()
+    {
+        _scrollTween = Tween.UIAnchoredPosition3DY(_creditsTransform,
+            _creditsLength, _scrollDuration, ease: Ease.Linear)
+                .OnComplete(() =>
+                {
+                    SceneController.Instance.LoadNewScene(0, SceneTransitionType.Black);
+                });
+    }
+}
